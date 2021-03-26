ae6dec0835a1e9c69491c851d51a1a8c1c21757b
Kestas Jasinevicius <kestasj@comcast.net>
2021-03-26 13:25:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace WebAutoCore
+{
+
+    public class WebTest
+    {
+
+        [Fact]
+        public void DemoTest()
+        {
+            IWebDriver chrome = new ChromeDriver(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), new ChromeOptions());
+            chrome.Manage().Window.Maximize();
+            chrome.Navigate().GoToUrl("https://www.google.com/");
+            chrome.Quit();
+        }
+
+
+    }
+}
